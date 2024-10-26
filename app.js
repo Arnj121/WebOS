d059dc637ba6d528d9e95b7f13f1da19549b4b23
Arnj121 <arnj121@gmail.com>
2024-10-26 11:46:54 -0500

    docker

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.post('/initapp',processor.initapp)
 app.post('/saveapp',processor.saveapp)
 app.get('/loadapps',processor.loadapps)
 app.post('/saveapps',processor.saveapps)
-app.listen(process.env.PORT || 2000,()=>{
+app.listen(process.env.PORT || 2000,process.env.IP,()=>{
     console.log('we are live on '+process.env.PORT || 2000)
 })
--- dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:18
-
-WORKDIR /app
-COPY . .
-EXPOSE 2000
-RUN npm install
-CMD node app
