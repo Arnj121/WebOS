41e376233bc926e21730d82253f679013d03d0ba
Arnj121 <arnj121@gmail.com>
2024-11-04 10:55:47 -0600

    appspec

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,8 +3,7 @@ cd /home/ec2-user/app
 npm install
 node initIP
 cd /home/ec2-user/
-mkdir fonty
-cd /home/ec2-user/fonty
 git clone https://github.com/arnj121/fonty
+cd /home/ec2-user/fonty
 npm install
 node initIP
