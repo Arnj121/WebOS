aa6674e349c03c3814cf6c321653d988ba7cdf17
Arnj121 <arnj121@gmail.com>
2024-11-04 19:14:15 -0600

    f

--- prepareSourceCode.sh
+++ prepareSourceCode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 zip source.zip JSON static .env app.js appspec.yml db.js FiEx.js install_dependencies.sh install_node.sh \
-package-lock.json package.json processor.js start_app.sh stop_app.sh
+package-lock.json package.json processor.js start_app.sh
 aws s3 cp source.zip s3://webos-534424/source.zip
--- start_app.sh
+++ start_app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/app
+pm2 stop app.js
 pm2 start app.js
 cd /home/ec2-user/fonty
+pm2 stop server.js
 pm2 start server.js
 
