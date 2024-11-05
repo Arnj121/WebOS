2500cd71d6ca34076b16b8e0d6d7d5c7385f58e4
Arnj121 <arnj121@gmail.com>
2024-11-04 18:31:12 -0600

    f

--- stop_app.sh
+++ stop_app.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cd /home/ec2-user/fonty
-pm2 stop server.js
-cd /home/ec2-user/app
-pm2 stop app.js
+if [ -d  "/home/ec2-user/fonty" ]; then
+  cd /home/ec2-user/fonty
+  pm2 stop server.js
+else
+  echo "doesn't exist"
+fi
+if [ -d  "/home/ec2-user/app" ]; then
+  cd /home/ec2-user/app
+  pm2 stop app.js
+else
+  echo "doesn't exist"
+fi
